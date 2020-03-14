0fb8c251c8fada3f5b7d95452901115f612da72b
ALEXEN <alexpf49@gmail.com>
2020-03-14 20:15:25 +0100

    La base esta acabada

--- src/main/java/com/company/Datagram/Carta.java
+++ src/main/java/com/company/Datagram/Carta.java
@@ -1,82 +1,114 @@
 package com.company.Datagram;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.company.Datagram.DatagramSocketClient.baraja;
+import static com.company.Datagram.DatagramSocketClient.portDesti;
 
 public class Carta {
-
-    public static List<String> listaTipos = new ArrayList<>();
-    public static List<Integer> listaValores = new ArrayList<>();
+    public static List<Carta> listaCartas = new ArrayList<>();
 
     String tipo;
     String valor;
+    public static int puntosTotales;
 
-
-    public Carta(String tipo, String valor){
+    public Carta(String valor, String tipo){
         this.tipo = tipo;
         this.valor = valor;
     }
 
+    public static void cojerCarta(){
+        int numCartaRandom = (int) ((Math.random() * (listaCartas.size())) + 0);
+        baraja.add(listaCartas.get(numCartaRandom));
+        System.out.println();
+        System.out.println("Te ha tocado un "+listaCartas.get(numCartaRandom).valor+"-"+listaCartas.get(numCartaRandom).tipo);
+        puntosTotales = 0;
 
-    public static Carta cojerCarta(){
-
-        int numValorCartaRandom = (int) ((Math.random() * (13)) + 1);
-        int numTipoCartaRandom = (int) ((Math.random() * (4)) + 0);
-        String valorRandom = "";
-        if (numValorCartaRandom<=10){
-            valorRandom = String.valueOf(listaValores.get(numValorCartaRandom)-1);
-        }else if (numValorCartaRandom==11){
-            valorRandom = "J";
-        }else if (numValorCartaRandom==12){
-            valorRandom = "Q";
-        }else if (numValorCartaRandom==13){
-            valorRandom = "K";
+        for (Carta carta : baraja) {
+            if (carta.valor.equals("J") || carta.valor.equals("Q") || carta.valor.equals("K")) {
+                puntosTotales += 10;
+            } else {
+                puntosTotales += Integer.parseInt(carta.valor);
+            }
         }
-
-        String tipoRandom = listaTipos.get(numTipoCartaRandom);
-
-        Carta carta = new Carta(tipoRandom, valorRandom);
-        System.out.println(carta);
-
-//        baraja.add(carta);
-        int i = 0;
-        if (baraja.isEmpty()){
-            baraja.add(carta);
-        }else {
-
-            String finalValorRandom = valorRandom;
-            String v = String.valueOf(baraja.stream()
-                    .filter(baraja->baraja.valor.equals(finalValorRandom))
-                    .count());
-            System.out.println(v);
-//            for (i = 0; i <=baraja.size(); i++) {
-//                if (!baraja.get(i).valor .equals(valorRandom)){
-//                    baraja.add(carta);
-//                    return carta;
-//                } else {
-//                    if (i==baraja.size()) {
-//                        break;
-//                    }
-//                }
-//            }
-        }
-        if (baraja.size()==i){
-            baraja.add(carta);
-        }
-
-        return carta;
+        System.out.println("Tienes "+puntosTotales+" puntos");
+        listaCartas.remove(numCartaRandom);
     }
-    public static void rellenar(){
-        listaTipos.add("Corazones");
-        listaTipos.add("Diamantes");
-        listaTipos.add("Picas");
-        listaTipos.add("Treboles");
 
-        for (int i = 1; i <=13 ; i++) {
-            listaValores.add(i);
+    public static List<Carta> rellenar(){
+        int countTrebol = 1;
+        int countPicas = 1;
+        int countCorazones = 1;
+        int countDiamantes = 1;
+
+
+        for (int i = 1; listaCartas.size()<= 51 ; i++) {
+
+            if (countTrebol<=13){
+                if (countTrebol == 11){
+                    listaCartas.add(new Carta("J","Trebol"));
+                    countTrebol++;
+                }else if (countTrebol == 12){
+                    listaCartas.add(new Carta("Q","Trebol"));
+                    countTrebol++;
+                }else if (countTrebol == 13){
+                    listaCartas.add(new Carta("K","Trebol"));
+                    countTrebol++;
+                }else if (countTrebol<=10){
+                    listaCartas.add(new Carta(String.valueOf(i),"Trebol"));
+                    countTrebol++;
+                }
+            }
+
+            if (countPicas<=13){
+                if (countPicas == 11){
+                    listaCartas.add(new Carta("J","Picas"));
+                    countPicas++;
+                }else if (countPicas == 12){
+                    listaCartas.add(new Carta("Q","Picas"));
+                    countPicas++;
+                }else if (countPicas == 13){
+                    listaCartas.add(new Carta("K","Picas"));
+                    countPicas++;
+                }else if (countPicas<=10){
+                    listaCartas.add(new Carta(String.valueOf(i),"Picas"));
+                    countPicas++;
+                }
+            }
+            if (countCorazones<=13){
+                if (countCorazones == 11){
+                    listaCartas.add(new Carta("J","Corazones"));
+                    countCorazones++;
+                }else if (countCorazones == 12){
+                    listaCartas.add(new Carta("Q","Corazones"));
+                    countCorazones++;
+                }else if (countCorazones == 13){
+                    listaCartas.add(new Carta("K","Corazones"));
+                    countCorazones++;
+                }else if (countCorazones<=10){
+                    listaCartas.add(new Carta(String.valueOf(i),"Corazones"));
+                    countCorazones++;
+                }
+            }
+            if (countDiamantes<=13){
+                if (countDiamantes == 11){
+                    listaCartas.add(new Carta("J","Diamantes"));
+                    countDiamantes++;
+                }else if (countDiamantes == 12){
+                    listaCartas.add(new Carta("Q","Diamantes"));
+                    countDiamantes++;
+                }else if (countDiamantes == 13){
+                    listaCartas.add(new Carta("K","Diamantes"));
+                    countDiamantes++;
+                }else if (countDiamantes<=10){
+                    listaCartas.add(new Carta(String.valueOf(i),"Diamantes"));
+                    countDiamantes++;
+                }
+            }
         }
+        return listaCartas;
     }
 
 
--- src/main/java/com/company/Datagram/DatagramSocketClient.java
+++ src/main/java/com/company/Datagram/DatagramSocketClient.java
@@ -9,10 +9,12 @@ import java.util.List;
 import java.util.Scanner;
 
 import static com.company.Datagram.Carta.*;
+import static java.lang.System.exit;
 
 public class DatagramSocketClient {
     public static InetAddress adrecaDesti;
     public static List<Carta> baraja = new ArrayList<>();
+    static String nombre;
     static {
         try {
             adrecaDesti = InetAddress.getByName("localhost");
@@ -24,141 +26,62 @@ public class DatagramSocketClient {
     public static int portDesti = 25565;
 
     public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
 
-          //cargando cartas
-//        cartas(cartas);
-//        pedirCarta(baraja);
-//        pedirCarta(baraja);
-//        pedirCarta(baraja);
-//        pedirCarta(baraja);
+        System.out.println("Cual es tu nombre?");
+        System.out.println();
+        nombre = in.nextLine();
+
+
         rellenar();
-        cojerCarta();
-        cojerCarta();
-        cojerCarta();
-        cojerCarta();
-        cojerCarta();
-        System.out.println(baraja.getClass().toString());
-        mostrarCartas();
 
+
+        while (true){
+            System.out.println();
+            System.out.println("Que quieres hacer?");
+            System.out.println();
+            System.out.println("1.-Pedir Carta\n2.-Mostrar Cartas");
+
+            int op = in.nextInt();
+
+            switch (op){
+                case 1:
+                    cojerCarta();
+                    if (puntosTotales > 21){
+                        System.out.println();
+                        System.out.println("Te has pasado, lo siento");
+                        mostrarCartas();
+                        exit(0);
+                    }
+                    break;
+                case 2:
+                    mostrarCartas();
+                    break;
+            }
+        }
     }
 
 
     public static void mostrarCartas() throws IOException {
-//        System.out.println(baraja);
-//
-//        for (int i = 0; i < 52; i++) {
-//
-//            byte[] missatge = cartas.;
-//
-//            //creació del paquet a enviar
-//            DatagramPacket packet = new DatagramPacket(missatge,
-//                    missatge.length,
-//                    adrecaDesti,
-//                    portDesti);
-//
-//            //creació d'un sòcol temporal amb el qual realitzar l'enviament
-//            DatagramSocket socket = new DatagramSocket();
-//
-//
-//            //Enviament del missatge
-//            socket.send(packet);
-//        }
-    }
-//    public static java.util.List<String> cartas(java.util.List<String>cartas){
-//
-//        int countTrebol = 1;
-//        int countPicas = 1;
-//        int countCorazones = 1;
-//        int countDiamantes = 1;
-//
-//
-//        for (int i = 1; cartas.size()<= 51 ; i++) {
-//
-//            if (countTrebol<=13){
-//                if (countTrebol == 11){
-//                    cartas.add("J-Trebol");
-//                    countTrebol++;
-//                }else if (countTrebol == 12){
-//                    cartas.add("Q-Trebol");
-//                    countTrebol++;
-//                }else if (countTrebol == 13){
-//                    cartas.add("K-Trebol");
-//                    countTrebol++;
-//                }else if (countTrebol<=10){
-//                    cartas.add(i+"-Trebol");
-//                    countTrebol++;
-//                }
-//            }
-//
-//            if (countPicas<=13){
-//                if (countPicas == 11){
-//                    cartas.add("J-Picas");
-//                    countPicas++;
-//                }else if (countPicas == 12){
-//                    cartas.add("Q-Picas");
-//                    countPicas++;
-//                }else if (countPicas == 13){
-//                    cartas.add("K-Picas");
-//                    countPicas++;
-//                }else if (countPicas<=10){
-//                    cartas.add(i+"-Picas");
-//                    countPicas++;
-//                }
-//            }
-//            if (countCorazones<=13){
-//                if (countCorazones == 11){
-//                    cartas.add("J-Corazones");
-//                    countCorazones++;
-//                }else if (countCorazones == 12){
-//                    cartas.add("Q-Corazones");
-//                    countCorazones++;
-//                }else if (countCorazones == 13){
-//                    cartas.add("K-Corazones");
-//                    countCorazones++;
-//                }else if (countCorazones<=10){
-//                    cartas.add(i+"-Corazones");
-//                    countCorazones++;
-//                }
-//            }
-//            if (countDiamantes<=13){
-//                if (countDiamantes == 11){
-//                    cartas.add("J-Diamantes");
-//                    countDiamantes++;
-//                }else if (countDiamantes == 12){
-//                    cartas.add("Q-Diamantes");
-//                    countDiamantes++;
-//                }else if (countDiamantes == 13){
-//                    cartas.add("K-Diamantes");
-//                    countDiamantes++;
-//                }else if (countDiamantes<=10){
-//                    cartas.add(i+"-Diamantes");
-//                    countDiamantes++;
-//                }
-//            }
-//        }
-//        return cartas;
-//    }
+        System.out.println(baraja);
+        byte[] missatge = " ".getBytes();
+            if (puntosTotales>21){
+                missatge = String.valueOf(nombre+" -> "+puntosTotales+" puntos -> HA PERDIDO").getBytes();
+            }else {
+                missatge = String.valueOf(nombre+" -> "+puntosTotales+" puntos").getBytes();
+            }
 
-//    public static java.util.List<String> pedirCarta(java.util.List<String> baraja){
-//
-//        int numCartaRandom = (int) ((Math.random() * (52)) + 0);
-//        if (baraja.isEmpty()){
-//            baraja.add(cartas.get(numCartaRandom));
-//        }else {
-//            for (int i  = 1; i != baraja.size()+1; i++) {
-//
-//                if (Integer.parseInt(String.valueOf(baraja.indexOf(i))) == numCartaRandom){
-//                    numCartaRandom = (int) ((Math.random() * (52)) + 0);
-//                }else {
-//                    baraja.add(cartas.get(numCartaRandom));
-//
-//                    break;
-//
-//                }
-//
-//            }
-//        }
-//
-//        return baraja;
-//    }
+            //creació del paquet a enviar
+            DatagramPacket packet = new DatagramPacket(missatge,
+                    missatge.length,
+                    adrecaDesti,
+                    portDesti);
+
+            //creació d'un sòcol temporal amb el qual realitzar l'enviament
+            DatagramSocket socket = new DatagramSocket();
+
+
+            //Enviament del missatge
+            socket.send(packet);
+    }
 }
